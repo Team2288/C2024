d1e5016287d75eabde93f4f6c38d42dc2991e3d9
Ian-VA <ianatroshchenko@gmail.com>
2024-02-27 18:25:53 -0800

    Create BeamBreakSensor.java

---
+++ src/main/java/frc/robot/sensors/BeamBreakSensor.java
@@ -0,0 +1,16 @@
+package frc.robot.sensors;
+
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class BeamBreakSensor {
+    DigitalInput dio;
+
+    public BeamBreakSensor(int id) {
+        this.dio = new DigitalInput(id);
+    }   
+
+    public boolean getNoteDetected() {
+        return this.dio.get();
+    }
+}
