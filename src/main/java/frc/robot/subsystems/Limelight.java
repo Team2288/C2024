3c4b5f044c997031263ebdae3aaf8436266a7ef7
mariohydra2288 <mchaikittirattana@gmail.com>
2024-03-09 12:21:56 -0800

    limelight

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,11 +1,10 @@
 package frc.robot.subsystems;
 
-
-
 import org.photonvision.PhotonCamera;
 import org.photonvision.common.hardware.VisionLEDMode;
 import org.photonvision.targeting.PhotonPipelineResult;
 
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -15,8 +14,8 @@ public class Limelight extends SubsystemBase {
     NetworkTable table;
     NetworkTableEntry tx, ty, ta, botpose;
     double x, y, area, pose, distance;
-    double mountAngle; //degree
-    double mountHeight, targetHeight; //inches
+    double mountAngle; //radians
+    double mountHeight, speakerTargetHeight, speakerGoalRange; //meters
 
     PhotonCamera limelight;
     PhotonPipelineResult result;
@@ -29,11 +28,16 @@ public class Limelight extends SubsystemBase {
         ty = table.getEntry("ty");
         ta = table.getEntry("ta");
         table.getValue("");
-        mountAngle = 15.0;
-        mountHeight = 25.125;
-        targetHeight = 55.625;
         */
-        //limelight = new PhotonCamera("photonvision");
+        // Mount information
+        mountAngle = Units.degreesToRadians(15.0);
+        mountHeight = Units.inchesToMeters(25.125);
+        // Height of the targeted april tag
+        speakerTargetHeight = Units.inchesToMeters(55.625);
+        // Desired range from the speaker
+        speakerGoalRange = Units.inchesToMeters(.0);
+
+        limelight = new PhotonCamera("ironman");
         //limelight.setLED(VisionLEDMode.kOn);
     }
 
@@ -55,12 +59,12 @@ public class Limelight extends SubsystemBase {
         //hasTargets = result.hasTargets();
     }
 
-    // Find the horizontal distance(inches) from a target given
+    // Find the horizontal distance(meters) from the speaker given
         // mount height(h1) and angle(a1), and target height(h2); a2(y)=angle from limelight to target(given by crosshair)
     public double distanceFromTarget() {
         //tan(a1+a2) = (h2-h1) / d  -->  d = (h2-h1) / tan(a1+a2)
         double angleToGoalRad = (mountAngle + y) * (Math.PI / 180);
-        return distance = (targetHeight - mountHeight) / Math.tan(angleToGoalRad);
+        return distance = (speakerTargetHeight - mountHeight) / Math.tan(angleToGoalRad);
     }
 
     // Post to smart dashboard
