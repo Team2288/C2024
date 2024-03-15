cf6ab58ffff602e143077eae1f31e9729f0aaf44
mariohydra2288 <mchaikittirattana@gmail.com>
2024-03-14 17:12:25 -0700

    auto aim test

---
+++ src/main/java/frc/robot/commands/AutoAlignSwerve.java
@@ -0,0 +1,54 @@
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.Constants;
+import frc.robot.LimelightHelpers;
+import frc.robot.subsystems.Swerve;
+
+public class AutoAlignSwerve extends Command{
+    private Swerve s_Swerve;    
+    private DoubleSupplier strafeSup;
+
+    public AutoAlignSwerve(Swerve s_Swerve, DoubleSupplier strafeSup) {
+        this.s_Swerve = s_Swerve;
+        addRequirements(s_Swerve);
+
+        this.strafeSup = strafeSup;
+    }
+
+    private Double limelightAimKP() {    
+        // if the robot never turns in the correct direction, kP should be inverted.
+        double kP = Constants.Swerve.angleKP;
+        double targetingAngularVelocity = LimelightHelpers.getTX("ironman") * kP;
+        //invert since tx is positive when the target is to the right of the crosshair
+        targetingAngularVelocity = -targetingAngularVelocity;
+        return targetingAngularVelocity;
+    }
+    private Double limelightRangeProportional() {    
+        double kP = Constants.Swerve.driveKP;
+        double targetingForwardSpeed = LimelightHelpers.getTY("ironman") * kP;
+        targetingForwardSpeed = -targetingForwardSpeed;
+        return targetingForwardSpeed;
+    }
+
+    @Override
+    public void execute() {
+        /* Get Values, Deadband*/
+        double translationVal = MathUtil.applyDeadband(limelightRangeProportional(), Constants.moveDeadband);
+        double strafeVal = MathUtil.applyDeadband(strafeSup.getAsDouble(), Constants.moveDeadband);
+        double rotationVal = MathUtil.applyDeadband(limelightAimKP(), Constants.rotateDeadband);
+
+        /* Drive */
+        s_Swerve.drive(
+            new Translation2d(translationVal, strafeVal).times(Constants.Swerve.maxSpeed), 
+            rotationVal * Constants.Swerve.maxAngularVelocity, 
+            false, 
+            true
+        );
+    }
+}
--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,54 +1,43 @@
 package frc.robot.subsystems;
 
-import org.photonvision.PhotonCamera;
-import org.photonvision.common.hardware.VisionLEDMode;
-import org.photonvision.targeting.PhotonPipelineResult;
-
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.LimelightHelpers;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
-import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonPoseEstimator.PoseStrategy;
-
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import java.util.Optional;
-import org.photonvision.EstimatedRobotPose;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Limelight extends SubsystemBase {
     NetworkTable table;
-    NetworkTableEntry tx, ty, ta, tl, botpose;
+    NetworkTableEntry tx, ty, ta, tl;
     double x, y, area, latency, distance;
     double mountAngle; //radians
     double mountHeight, speakerTargetHeight, speakerGoalRange; //meters
 
     Transform3d robotToCam = new Transform3d(new Translation3d(0, 0.0254, 0.584), new Rotation3d(0, 0, 0));
-    PhotonCamera limelight;
-    PhotonPoseEstimator poseEstimator;
-    PhotonPipelineResult result;
     boolean hasTargets;
-    AprilTagFieldLayout aprilTagFieldLayout;
+    //AprilTagFieldLayout aprilTagFieldLayout;
 
     LimelightHelpers.PoseEstimate pose;
 
     public Limelight() {
-        
+        // Create network instance of camera and values it supplies
         table = NetworkTableInstance.getDefault().getTable("limelight-ironman");
-        tx = table.getEntry("tx");
-        ty = table.getEntry("ty");
-        ta = table.getEntry("ta");
-        tl = table.getEntry("tl");
-        table.getValue("");
+        tx = table.getEntry("tx"); // horizontal distance from crosshair to center of target
+        ty = table.getEntry("ty"); // vertical distance from crosshair to center of target
+        ta = table.getEntry("ta"); // area target takes up
+        tl = table.getEntry("tl"); // camera latency
 
         // Mount information
         mountAngle = Units.degreesToRadians(15.0);
@@ -56,10 +45,10 @@ public class Limelight extends SubsystemBase {
         // Height of the targeted april tag
         speakerTargetHeight = Units.inchesToMeters(55.625);
         // Desired range from the speaker
-        speakerGoalRange = Units.inchesToMeters(.0);
-        aprilTagFieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
+        speakerGoalRange = Units.inchesToMeters(.0); // TO-DO
+        //aprilTagFieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
 
-        //poseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, limelight, robotToCam);
+        // Current pose of the robot relative to the blue alliance as the origin
         pose = LimelightHelpers.getBotPoseEstimate_wpiBlue("ironman");
     }
 
@@ -78,8 +67,6 @@ public class Limelight extends SubsystemBase {
         SmartDashboard(); 
         
         pose = LimelightHelpers.getBotPoseEstimate_wpiBlue("ironman");
-        //result = limelight.getLatestResult();
-        //hasTargets = result.hasTargets();
     }
 
 
